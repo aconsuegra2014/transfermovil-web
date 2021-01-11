23672b576f60254923f1c46bfcb259c652cdf548
Arley Consuegra Roselló <arley.consuegra@electronicid.eu>
2021-01-10 20:05:53 -0500

    Registrando tarjeta BPA

---
+++ main.js
@@ -0,0 +1,17 @@
+
+const bpaCard = document.querySelector('input#bpa-new-card');
+bpaCard.value = "";
+
+bpaCard.addEventListener('input', setCard);
+bpaCard.addEventListener('keyup', formatCard);
+
+function setCard(e) {
+    let addCard = document.querySelector("a#bpa-add-card")
+    let cardNumber = bpaCard.value
+    addCard.href=`tel:*444*49*01*${cardNumber}#`
+
+}
+
+function formatCard(e){
+    this.value = this.value.replace(/ /g, "").replace(/(\d{4})/g, '$1 ').replace(/(^\s+|\s+$)/,'');
+}
